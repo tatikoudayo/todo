b2ef558018b786a8411f8f83dd7aa466ca369668
Koki Tachibana <tachibana.koki.ti0@is.naist.jp>
2020-12-28 23:21:08 +0900

    modify js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -3,6 +3,11 @@ function App() {
     return (
         <Container>
             <Row>
+                <Col>
+                    <h1> Todo App</h1>
+                </Col>
+            </Row>
+            <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
                 </Col>
